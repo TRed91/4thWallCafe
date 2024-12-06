77b2c53764f909d10327d7e6f55a4597a7bd5f16
TRed91 <thomas.roth@aon.at>
2024-12-07 00:22:25 +0100

    update design document

--- 4thWallCafe.Core/obj/Debug/net8.0/4thWallCafe.Core.AssemblyInfo.cs
+++ 4thWallCafe.Core/obj/Debug/net8.0/4thWallCafe.Core.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4thWallCafe.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b5d3e151a1edc5c96b2b042fcd57e6b33d20c04")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e5c25ac8c7b9d12b868d4e35f8d5c3d8ea887dea")]
 [assembly: System.Reflection.AssemblyProductAttribute("4thWallCafe.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4thWallCafe.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
