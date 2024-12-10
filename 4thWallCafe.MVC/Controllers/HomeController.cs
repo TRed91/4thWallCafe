f429efc311ad6fd5c608b6091bfae18ba4ca6626
TRed91 <thomas.roth@aon.at>
2024-12-10 14:58:52 +0100

    add user authentication to mvc
    created controller, actions and views for user
    authentication

---
+++ 4thWallCafe.MVC/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _4thWallCafe.MVC.Controllers;
+
+public class HomeController : Controller
+{
+    [Authorize]
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
---
+++ 4thWallCafe.MVC/db/IdentityModels/LoginUser.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace _4thWallCafe.MVC.db.IdentityModels;
+
+public class LoginUser
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    
+    public bool RememberMe { get; set; }
+}
