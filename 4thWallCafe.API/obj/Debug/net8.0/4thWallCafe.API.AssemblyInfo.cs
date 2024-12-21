487b4bf4ee38939f036061558f270875e30174ea
TRed91 <thomas.roth@aon.at>
2024-12-21 20:17:35 +0100

    update design document

--- 4thWallCafe.API/obj/Debug/net8.0/4thWallCafe.API.AssemblyInfo.cs
+++ 4thWallCafe.API/obj/Debug/net8.0/4thWallCafe.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("4thWallCafe.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da98f312bd65bd036227ced9471cad219fe783b3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d1c17701e19897fc49aa6a897b070731f3df7a7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("4thWallCafe.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("4thWallCafe.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
